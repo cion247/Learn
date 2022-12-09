0ab7e829be0350848d1e21fb71e76063137696d9
cion247 <96902253+cion247@users.noreply.github.com>
2022-12-09 20:29:47 +0530

    day 2

--- src/Interfacing/Main.java
+++ src/Interfacing/Main.java
@@ -2,11 +2,14 @@ package Interfacing;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello ");
         ITelephone timsPhone ;
         timsPhone = new DeskPhone(123456);
         timsPhone.powerOn();
         timsPhone.callPhone(123456);
+        timsPhone.answer();
+
+        timsPhone = new MobilePhone(24565);
+        timsPhone.callPhone(24565);
         timsPhone.answer();
 
     }
